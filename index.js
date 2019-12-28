1c1d4c1cf0e21a972389fc1d985a4d0b747b94f5
Greg Roberts <groberts3104@gmail.com>
2019-12-28 16:40:36 -0500

    installed axios npm

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const inquirer = require("Inquirer");
 const fs = require("fs");
+const axios = require("axios");
+const util = require("util");
+var pdf = require("html-pdf");
+var html = fs.readFileSync('index.html', 'utf8');
+var options = { format: 'Letter' };
+
+
+const readFileAsync = util.promisify(fs.readFile);
+
